5b21c5f2c16d76ae484010f545f5900e20a12f5d
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2024-08-09 01:37:10 +0530

    changelog: created the catalogue page created the catelogue root template

---
+++ src/components/catalogue/option.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface OptionProps {
+  categoryName: string
+}
+
+const Option = ({ categoryName }: OptionProps) => {
+  return (
+    <div className="group relative overflow-hidden shadow-md">
+      <Link href={`/catalogue/${categoryName}`}>
+        <Image
+          alt={categoryName}
+          className="scale-125 transition duration-300 ease-in-out group-hover:scale-105"
+          height={500}
+          src={`/images/${categoryName}.png`}
+          width={500}
+        />
+        <div className="absolute bottom-0 left-0 flex h-0 w-full flex-col justify-end overflow-hidden bg-gradient-to-t from-black px-12 text-center text-sm duration-300 ease-in-out group-hover:h-1/2 group-hover:py-4">
+          <h3 className="text-xl font-medium text-custT">{categoryName}</h3>
+          {/* <p>View our {categoryName} collection</p> */}
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Option
