bc8bbe96d567fd8191e6de52c8c7e078af55b388
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2024-08-17 18:26:32 +0530

    changelog: made the catalogue page and it's components, fully responsive for all screen sizes updated a dummy image filename in public dir updated some href links in links.json

--- src/components/catalogue/option.tsx
+++ src/components/catalogue/option.tsx
@@ -3,23 +3,28 @@ import Link from 'next/link'
 
 interface OptionProps {
   categoryName: string
+  link: string
 }
 
-const Option = ({ categoryName }: OptionProps) => {
+const Option = ({ categoryName, link }: OptionProps) => {
   return (
     <div className="group relative overflow-hidden">
       <Link
-        href={`/catalogue/${categoryName}`}
+        className="flex h-36 sm:h-60 md:h-72 lg:h-80 xl:h-60"
+        href={`/catalogue/${link}`}
       >
         <Image
+          fill
           alt={categoryName}
           className="scale-125 transition duration-300 ease-in-out group-hover:scale-105"
-          height={500}
+          objectFit="cover"
+          sizes="(max-width: 1280px) 50vw, 25vw"
           src={`/images/${categoryName}.png`}
-          width={500}
         />
-        <div className="absolute bottom-0 left-0 flex h-0 w-full flex-col justify-end overflow-hidden bg-gradient-to-t from-black px-12 text-center text-sm duration-300 ease-in-out group-hover:h-1/2 group-hover:py-4">
-          <h1 className="text-xl font-medium text-60">{categoryName}</h1>
+        <div className="absolute bottom-0 left-0 flex h-0 w-full flex-col justify-end overflow-hidden bg-gradient-to-t from-black px-12 text-center text-sm duration-300 ease-in-out group-hover:h-1/2 group-hover:py-3 md:group-hover:py-6 lg:group-hover:py-8 xl:group-hover:py-6">
+          <h1 className="font-medium text-60 md:text-2xl lg:text-3xl xl:text-xl">
+            {categoryName}
+          </h1>
         </div>
       </Link>
     </div>
