afa844d632300d25c799199225bdefddfe1f256d
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2024-08-19 12:45:17 +0530

    updated next.config api routes proxy for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `https://interior-exterior-designer-392dr7l1q-tarun-choudhurys-projects.vercel.app/:path*`,
+        },
+      ],
+    }
+  },
   images: {
     remotePatterns: [
       {
