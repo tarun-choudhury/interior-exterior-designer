08cdc1984dcb05828384cb8e6e8ffba1b858a45d
tanmoy-OG <official.tanmoychoudhury@protonmail.com>
2024-08-31 22:58:03 +0530

    added a what-do-we-do section as a component in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import CallToAction from '@/components/home/call-to-action'
 import Catchphrase from '@/components/home/catchphrase'
 import CustomerBrands from '@/components/home/customer-brands'
 import EmblaCarousel from '@/components/home/hero-carousel'
+import WhatDoWeDo from '@/components/home/what-do-we-do'
 
 const Home = () => {
   const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
@@ -16,6 +17,7 @@ const Home = () => {
       <div className="flex flex-col items-center justify-between gap-10 p-4 pt-0 sm:gap-20 sm:p-10 md:p-20 md:pt-0">
         <Catchphrase />
         <CustomerBrands />
+        <WhatDoWeDo />
         <CallToAction />
       </div>
     </main>
